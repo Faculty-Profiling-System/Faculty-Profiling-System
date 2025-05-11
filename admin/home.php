2eb742abd3af10daaa8d4bbf443efd44e096251d
kurt <fajutagana_johnkurt@plpasig.edu.ph>
2025-05-11 21:21:52 +0800

    updated home.php and blahblah

--- admin/home.php
+++ admin/home.php
@@ -202,13 +202,13 @@ session_start();
       <nav>
         <ul>
           <li><a href="home.php" class="active"><img src="../images/home.png" alt="Home Icon" class="menu-icon">HOME</a></li>
-          <li><a href="department.php"><img src="../images/department.png" alt="Department Icon" class="menu-icon">DEPARTMENT MANAGEMENT</a></li>
+          <li><a href="department.php"><img src="../images/department.png" alt="Department Icon" class="menu-icon">COLLEGE MANAGEMENT</a></li>
           <li><a href="user.php"><img src="../images/user.png" alt="User Icon" class="menu-icon">USER MANAGEMENT</a></li>
           <li class="dropdown">
             <a href="javascript:void(0)" id="reportsDropdown"><img src="../images/reports.png" alt="Reports Icon" class="menu-icon">REPORTS</a>
             <ul class="dropdown-menu">
-              <li><a href="files_report.php">Files</a></li>
-              <li><a href="logs_report.php">Logs</a></li>
+              <li><a href="files_report.php">CREDENTIAL FILES</a></li>
+              <li><a href="logs_report.php">USER LOGS</a></li>
             </ul>
           </li>
           <li><a href="setting.php"><img src="../images/setting.png" alt="Settings Icon" class="menu-icon">SETTINGS</a></li>
