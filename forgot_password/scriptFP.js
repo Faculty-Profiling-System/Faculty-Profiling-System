1f0b50609141e450f09fc4f5bf684ae1ad694b1b
kurt <fajutagana_johnkurt@plpasig.edu.ph>
2025-05-15 00:46:27 +0800

    deleted folders

--- forgot_password/scriptFP.js
+++
@@ -1,9 +0,0 @@
-function resetLink() {
-    const email = document.getElementById("email").value;
-    if (email.trim() === "") {
-      alert("Please enter a valid email address.");
-    } else {
-      alert("Password reset link has been sent to " + email);
-    }
-  }
-  
